9479d84bd469c160f77ddbbc1b87380787ba51e3
Cameron <cgree78@gmail.com>
2016-04-29 17:13:48 -0500

    Added "Hero" names to all images in the json

--- app.js
+++ app.js
@@ -2,18 +2,20 @@
 
 // Declare app level module which depends on views, and components
 angular.module('OverPapers', ['ngAnimate'])
-    .controller('heroController', function($scope) {
+    .controller('heroController', function ($scope) {
         $scope.heroes = ["Bastion", "D.Va", "Genji", "Hanzo", "Junkrat", "Lucio", "McCree", "Mei", "Mercy", "Pharah", "Reaper", "Reinheart", "Roadhog", "Soldier 76", "Symmetra", "Torbjorn", "Tracer", "Widowmaker", "Winston", "Zarya", "Zenyatta"];
     })
-    .controller('overController', function($scope, $http) {
+    .controller('overController', function ($scope, $http) {
         $scope.content = null;
         $http.get('wallpapers_info.json')
-            .success(function(data, status, headers, config) {
+            .success(function (data, status, headers, config) {
                 $scope.contents = data;
             })
-            .error(function(data, status, headers, config) {});
-        $scope.openImage = function () {
-            console.log($scope.content.src);
+            .error(function (data, status, headers, config) {
+            });
+        $scope.openImage = function (src) {
+            console.log(src);
+            window.location.href = src;
         }
-        
+
     });
