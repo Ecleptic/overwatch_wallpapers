294f12b3a2b1dbddebdc55dced21389805e370aa
Cameron <cgree78@gmail.com>
2016-04-29 21:01:33 -0500

    With Coltex's help got search function working correctly

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
+// http://rawgit.com/Ecleptic/overwatch_wallpapers/master/wallpapers_info.json
 'use strict';
 
 // Declare app level module which depends on views, and components
 angular.module('OverPapers', ['ngAnimate'])
     .controller('heroController', function ($scope) {
-        $scope.heroes = ["Bastion", "D.Va", "Genji", "Hanzo", "Junkrat", "Lucio", "McCree", "Mei", "Mercy", "Pharah", "Reaper", "Reinheart", "Roadhog", "Soldier 76", "Symmetra", "Torbjorn", "Tracer", "Widowmaker", "Winston", "Zarya", "Zenyatta"];
+        $scope.heroes = ["Bastion", "D.Va", "Genji", "Hanzo", "Junkrat", "Lucio",
+            "McCree", "Mei", "Mercy", "Pharah", "Reaper", "Reinheart", "Roadhog",
+            "Soldier 76", "Symmetra", "Torbjorn", "Tracer", "Widowmaker", "Winston",
+            "Zarya", "Zenyatta"];
     })
     .controller('overController', function ($scope, $http) {
         $scope.content = null;
+        $scope.matchedImages = []
         $http.get('wallpapers_info.json')
             .success(function (data, status, headers, config) {
                 $scope.contents = data;
+                $scope.matchedImages = [];
             })
             .error(function (data, status, headers, config) {
             });
         $scope.openImage = function (src) {
             console.log(src);
             window.location.href = src;
-        }
+        };
 
+
+        $scope.searchTextChanged = function () {
+            $scope.matchedImages = $scope.contents.filter(function (item) {
+                if (!$scope.searchText || !$scope.searchText.length)
+                    return false
+
+                if (item.artist && item.artist.toUpperCase().indexOf($scope.searchText.toUpperCase()) !== -1)
+                    return true
+
+                for (var i = 0; i < item.tags.length; i++) {
+                    if (item.tags && item.tags[i].toUpperCase().indexOf($scope.searchText.toUpperCase()) !== -1)
+                        return true
+                }
+
+                return false
+            })
+        }
     });
+
+
