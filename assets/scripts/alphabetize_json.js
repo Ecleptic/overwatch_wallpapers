65ca931b7bb091e59fab358e898d01b1b1de9058
Cameron <cgree78@gmail.com>
2016-05-20 16:20:53 -0500

    small edits and alphabetization

--- assets/scripts/alphabetize_json.js
+++ assets/scripts/alphabetize_json.js
@@ -1,3 +1,10 @@
+/*
+
+ This script should be run from it's origin in '/overwatch_wallpapers/assets/scripts/alphabetize_json.js'
+
+ */
+
+
 var jsonfile = require('jsonfile')
 var file = '../../wallpapers_info.json'
 // var file2 = 'wallpapers_info2.json'
@@ -15,15 +22,15 @@ function sorting(json_object, key_to_sort_by) {
     json_object.sort(sortByKey);
     // console.log(json_object);
     //
-    jsonfile.writeFile(file,json_object, {spaces: 2},function (err) {
-      console.log(err);
+    jsonfile.writeFile(file, json_object, {spaces: 2}, function (err) {
+        console.log(err);
     })
 
 }
 
 
-jsonfile.readFile(file, function(err, obj) {
-  // console.dir(obj)
-  newobj = sorting(obj, 'src');
-  // console.log(obj);
+jsonfile.readFile(file, function (err, obj) {
+    // console.dir(obj)
+    newobj = sorting(obj, 'src');
+    // console.log(obj);
 });
